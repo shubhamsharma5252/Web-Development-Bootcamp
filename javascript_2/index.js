36df0fbab2aaa195d19f31bbada7b4500dd9652a
Shubham Sharma <shubham.sharma52525252@gmail.com>
2023-09-04 14:22:51 +0530

    second commit

---
+++ javascript_2/index.js
@@ -0,0 +1,8 @@
+console.log("javascript")
+//var a="45";
+//var a = 45 
+const author="shubham"//let author="faizan" throws an error because constant can not be changed
+author = "rohan"  
+console.log( author )
+
+
